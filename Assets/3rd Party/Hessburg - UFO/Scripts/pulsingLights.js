c49e20cfb37797fad27f1575a77b8390bf41d58c
fjunqueira <fabio.blumm18@gmail.com>
2016-12-24 00:50:47 -0200

    UFO idle animation

---
+++ Assets/3rd Party/Hessburg - UFO/Scripts/pulsingLights.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+var ufoMaterial : Material;
+private var ufoLightIntensity : float;
+private var saveOriginalColor : Color;
+
+function Start()
+{
+	if(ufoMaterial)
+	{	
+		ufoMaterial.EnableKeyword("_EMISSION");
+		saveOriginalColor=ufoMaterial.GetColor("_EmissionColor");
+	}
+}
+
+function Update () 
+{
+	if(ufoMaterial)
+	{
+		ufoLightIntensity=Mathf.PingPong(Time.time*0.5, 0.8);
+		ufoLightIntensity+=0.2;
+		ufoMaterial.SetColor("_EmissionColor", Color(ufoLightIntensity, ufoLightIntensity, ufoLightIntensity, 1.0));
+	}
+}
+
+function OnApplicationQuit()
+{
+	if(ufoMaterial) ufoMaterial.SetColor("_EmissionColor", saveOriginalColor);
+}	
