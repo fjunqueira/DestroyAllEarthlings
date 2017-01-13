b411c6f836c1e8b0b369d053a94bdbf241b193f6
fjunqueira <fabio.blumm18@gmail.com>
2017-01-13 00:24:26 -0200

    camera controls

--- Assets/Scripts/Components/UFO.cs
+++ Assets/Scripts/Components/UFO.cs
@@ -19,13 +19,13 @@ namespace SpaceCentipedeFromHell
 
             var input = Vector3.zero;
 
-            if (Input.GetKey(KeyCode.W)) input.y = -1;
+            if (Input.GetKey(KeyCode.W)) input.y = 1;
 
-            if (Input.GetKey(KeyCode.A)) input.x = -1;
+            if (Input.GetKey(KeyCode.A)) input.x = 1;
 
-            if (Input.GetKey(KeyCode.S)) input.y = 1;
+            if (Input.GetKey(KeyCode.S)) input.y = -1;
 
-            if (Input.GetKey(KeyCode.D)) input.x = 1;
+            if (Input.GetKey(KeyCode.D)) input.x = -1;
 
             if (input == Vector3.zero)
             {
---
+++ Assets/Scripts/Components/UFOCamera.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceCentipedeFromHell
+{
+    public class UFOCamera : MonoBehaviour
+    {
+        [SerializeField]
+        private UFO ufo;
+
+        private float angle = 180;
+
+		private bool startRotatingS = false;
+		
+		private bool startRotatingW = false;
+
+        private void Start()
+        {
+            //neededRotation = Quaternion.AngleAxis(180, ufo.transform.up);
+            //transform.RotateAround(ufo.transform.position, ufo.transform.up, 180);
+        }
+
+        private void Update()
+        {
+            //transform.rotation = Quaternion.RotateTowards(transform.rotation, neededRotation, Time.deltaTime * 100f);
+
+			if (Input.GetKey(KeyCode.S)) startRotatingS = true;
+			if (Input.GetKey(KeyCode.W)) startRotatingW = true;
+
+            if (!startRotatingW && startRotatingS && angle < 180)
+            {
+                transform.RotateAround(ufo.transform.position, ufo.transform.up, 10);
+                angle += 10;
+            }
+			else startRotatingS = false;
+
+			if (!startRotatingS && startRotatingW && angle > 0)
+            {
+                transform.RotateAround(ufo.transform.position, ufo.transform.up, -10);
+                angle -= 10;
+            }
+			else startRotatingW = false;
+
+			Debug.Log(angle);
+        }
+    }
+}
