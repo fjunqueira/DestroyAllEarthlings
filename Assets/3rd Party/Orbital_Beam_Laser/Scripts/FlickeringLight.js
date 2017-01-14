3f0898e41818bf11e9597bc1a80057c84b0b26d7
fjunqueira <fabio.blumm18@gmail.com>
2017-01-14 16:36:58 -0200

    Laser Asset

---
+++ Assets/3rd Party/Orbital_Beam_Laser/Scripts/FlickeringLight.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+var fuseLight : Light;
+private var fuseLightIntensity : int = 10;
+
+function Start () {
+
+}
+
+function Update () {
+
+    fuseLightIntensity = (Random.Range (5, 14));
+    fuseLight.intensity = fuseLightIntensity;
+
+}
